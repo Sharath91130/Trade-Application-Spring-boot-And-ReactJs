775f181fab4cfd94daaee9396548ed5d9300b432
Sharath91130 <sharathkurer8055@gmail.com>
2025-09-19 15:36:34 +0530

    kyc propose

---
+++ CustomerService/src/test/java/com/sharath/trade/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharath.trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
