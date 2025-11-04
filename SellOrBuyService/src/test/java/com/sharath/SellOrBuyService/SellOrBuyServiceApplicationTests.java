25b3641e4ece0a6677f312b748f1cb06b8e7ba4a
Sharath91130 <sharathkurer8055@gmail.com>
2025-11-04 11:01:36 +0530

    check

---
+++ SellOrBuyService/src/test/java/com/sharath/SellOrBuyService/SellOrBuyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharath.SellOrBuyService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellOrBuyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
