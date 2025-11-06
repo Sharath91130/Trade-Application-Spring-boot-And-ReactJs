566fbe478ea7d0fed91f40c42a874ea04dcc2eb1
Kanakappa Meti <1545@modus.local1>
2025-11-06 11:17:31 +0530

    kanaka 3rd commit

--- SellOrBuyService/src/main/java/com/modus/stock/SellOrBuyServiceApplication.java
+++ SellOrBuyService/src/main/java/com/modus/stock/SellOrBuyServiceApplication.java
@@ -1,5 +1,6 @@
 package com.modus.stock;
 
+import com.modus.stock.service.MarketPriceService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,6 +12,10 @@ public class SellOrBuyServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SellOrBuyServiceApplication.class, args);
+        MarketPriceService m=new MarketPriceService();
+        System.out.println(m.getCurrentPrice());
 	}
+
+
 
 }
